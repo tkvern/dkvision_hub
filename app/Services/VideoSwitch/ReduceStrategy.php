d2e429f3b34387234e9cae3a353268f24ee41084
liujun <lj90214@gmail.com>
2016-09-09 16:32:27 +0800

    修正路径错误

--- app/Services/VideoSwitch/ReduceStrategy.php
+++ app/Services/VideoSwitch/ReduceStrategy.php
@@ -26,27 +26,23 @@ class ReduceStrategy implements  Strategy {
      * @return array
      */
     protected function mapTask($task) {
-        $payload = json_decode($task->payload);
+        $payload = $task->payload;
         $start_frame = $payload['start_frame'];
         $end_frame = $payload['end_frame'];
         $total_frame = $end_frame - $start_frame;
         $subTasks = [];
         if($total_frame > 5) {
             for($i = $start_frame; $i < $end_frame; $i += 5) {
-                $tmpPayload = clone($payload);
+                $tmpPayload = array_copy($payload);
                 $tmpPayload['start_frame'] = $i;
                 $tmpPayload['end_frame'] = $i + 5 > $end_frame ? $end_frame : $$i + 1;
-                $subTask = $task->clone();
+                $subTask = $task->replicate(['uuid', 'payload', 'parent_id']);
                 $subTask->uuid = uuid1();
-                $subTask->payload = json_encode($tmpPayload);
+                $subTask->payload = $tmpPayload;
                 $subTask->parent_id = $task->id;
                 $subTasks[] = $subTask;
             }
-            DB::transaction(function() use ($subTasks) {
-                array_map(function($subTask) {
-                    $subTask->save();
-                }, $subTasks);
-            });
+            transaction_save_many($subTasks);
         } else {
             $subTasks[] = $task;
         }
